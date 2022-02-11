3b22bd96949ba8fc7459807b5c78872020a99082
Iain McWhinnie <2566283M@student.gla.ac.uk>
2022-02-11 13:02:00 +0000

    Adversary Implemenation

---
+++ src/QuickSortAdversary.java
@@ -0,0 +1,16 @@
+
+public class QuickSortAdversary {
+	
+	public static int[] generateAdversary(int n) {
+		int[] a = new int[n];
+		int middle = (n+1)/2;
+		for(int i=0; i<middle; i++) {
+			a[i] = 2*i;
+		}
+		for(int j=0; j<n-middle; j++) {
+			a[middle+j] = (2*j)+1;
+		}
+		return a;
+	}
+	
+}
