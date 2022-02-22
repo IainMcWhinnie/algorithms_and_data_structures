f7a09eecd4918824d2311cb5ff5724bc246dda6c
Iain McWhinnie <2566283M@student.gla.ac.uk>
2022-02-22 23:16:20 +0000

    Fixed adversary

--- src/QuickSortAdversary.java
+++ src/QuickSortAdversary.java
@@ -2,35 +2,6 @@
 public class QuickSortAdversary {
 	
 	public static int[] generateAdversary(int n) {
-<<<<<<< HEAD
-		int[] A = new int[n];
-		int cur = n-1;
-		int i = 0;
-		while (cur >= 0) {
-			A[i] = cur;
-			cur = cur - 2;
-			i++;
-		}
-		cur = 1 - (cur+2);
-		while (cur < n) {
-			A[i] = cur;
-			cur = cur + 2;
-			i++;
-		}
-		return A;
-	}
-	
-	
-	public static void main(String[] args) {
-		int[] a = generateAdversary(10);
-		
-		QuickSortMedianOfThreeMethod quickSort = new QuickSortMedianOfThreeMethod();
-		
-		System.out.println("Let see how long this takes");
-		quickSort.sort(a, 0, a.length-1);
-		System.out.println("DOnE");
-	}
-=======
 		int[] a = new int[n];
 		int middle = (n+1)/2;
 		for(int i=0; i<middle; i++) {
@@ -41,6 +12,5 @@ public class QuickSortAdversary {
 		}
 		return a;
 	}
-	
->>>>>>> 554ffa3c10eb45b1c986104a79516536a1428140
+
 }
