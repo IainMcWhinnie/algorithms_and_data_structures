5574aa8ba2ed5b5b939624409d3d499bc354f542
Iain McWhinnie <2566283M@student.gla.ac.uk>
2022-03-02 15:59:56 +0000

    Bit of progress

--- src/dataStructures/BST.java
+++ src/dataStructures/BST.java
@@ -99,19 +99,107 @@ public class BST {
 		
 	}
 	
-	public int[] inOrder() {
-		List<Integer> result = new ArrayList<>();
+	public int size(Node n) {
+		 int size = 0;
+		 
+		 if (n.left != null) {
+			 size += size(n.left);
+		 }
+		 if (n.right != null) {
+			 size += size(n.right);
+		 }
+		 return size+1;	
+	}
+	
+	public void inOrder(Node x) {
+		if (x!= null) {
+			inOrder(x.left);
+			System.out.print(x.key);
+			inOrder(x.right);
+		}
+	}
+	
+	public void preOrder(Node x) {
+		if (x!= null) {
+			System.out.print(x.key);
+			inOrder(x.left);
+			inOrder(x.right);
+		}
+	}
+
+	public void postOrder(Node x) {
+		if (x!= null) {
+			inOrder(x.left);
+			inOrder(x.right);
+			System.out.print(x.key);
+		}
+	}
+	
+	public String level(Node[] nodes) {
+		if (nodes.length == 0) {
+			return "";
+		}
+		
+		String out = "";
+		int noNodes = 0;
+		List<Node> children = new ArrayList<>();
+		for (Node node: nodes) {
+			
+			if (node != null) {
+				out += " "+Integer.toString(node.key)+" ";
+				children.add(node.left);
+				children.add(node.right);
+				noNodes ++;
+			}else {
+				out += " ".repeat(3);
+				children.add(null);
+			}
+		}
+		if (noNodes == 0) {
+			return "";
+		}
+		return out+"\n"+level(children.toArray(new Node[children.size()]));
+	}
+	
+	public void print() {
+		Node[] t = {root};
+		System.out.println(level(t));
+	}
+	
+	public boolean checkGreater(Node x, int value) {
+		
+	}
+	
+	public boolean checkLesser(Node x, int value) {
+		
+	}
+	
+	public boolean checkBST(Node x) {
 		
 		
-		return  result.toArray(new int[result.size()]);
+		
+		if (x.left != null && x.left.key > x.key) {
+			return false;
+		}
+		if (x.right != null && x.right.key < x.key) {
+			return false;
+		}
+		
+		
+		return true;
 	}
 	
 	
 	public static void main(String args[]) {
 		BST x = new BST();
-		int[] test = {1,4,2,6,3,7,5,8};
+		int[] test = {5,4,6,7,1,2,3,8};
 		for (int i: test) {
 			x.insert(i);
 		}
+		x.print();
+		System.out.println(x.size(x.root));
+		x.inOrder(x.root);
+		x.preOrder(x.root);
+		x.postOrder(x.root);
 	}
 }
